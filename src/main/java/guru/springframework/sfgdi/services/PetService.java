1c4102d162a6699f666bfe8bfe1a14ed41a8dc95
cosic04 <palacinke456>
2021-07-08 15:30:19 +0200

    Assignment 4: Dependency Injection Assignment

--- src/main/java/guru/springframework/sfgdi/services/PetService.java
+++ src/main/java/guru/springframework/sfgdi/services/PetService.java
@@ -1,5 +1,8 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+
 /**
  * Created by jt on 12/28/19.
  */
